109208fbfbfb07d5102a0a3ecb5639e9e8a97f32
Maelys Boennec <maelys.boennec@hotmail.fr>
2021-12-03 12:28:04 +0100

    code drivers

--- analyses/pop_change_drivers.R
+++ analyses/pop_change_drivers.R
@@ -4,6 +4,8 @@
 ## Population change drivers
 ################################################################################
 
+LPI.mod <- left_join(LPI.mod,temp_diff(), by=c("lat","long"))
+
 
 # Testing biomes impacts --------------------------------------------------
 mod_biome <- lm(slope ~ biome, data = LPI.mod)
@@ -14,6 +16,8 @@ mod_class <- lm(slope ~ Class, data = LPI.mod)
 p_mod_class <- anova(mod_class)[1,5]
 
 # Testing climate change impacts ------------------------------------------
-mod_v1 <- lm(slope ~ v1, data = LPI.mod)
-mod_v2 <- lm(slope ~ v2, data = LPI.mod)
-mod_v3 <- lm(slope ~ v3, data = LPI.mod)
+mod_tmax <- lm(slope ~ temp_diff_tasmax, data = LPI.mod) # Tmax impact
+p_mod_tmax <- anova(mod_tmax)[1,5]
+
+mod_tmin <- lm(slope ~ temp_diff_tasmin, data = LPI.mod) # Tmin impact
+p_mod_tmin <- anova(mod_tmin)[1,5]
